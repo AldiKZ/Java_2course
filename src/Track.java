73ebe72b440d19e244c1d1b9cef70fdee91ca0b9
AldikRkh <aldiyar.357@gmail.com>
2021-12-21 21:51:44 +0600

    cl

---
+++ src/Track.java
@@ -0,0 +1,11 @@
+public class Track {
+    private int distance = 50;
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+}
---
+++ src/Wall.java
@@ -0,0 +1,11 @@
+public class Wall {
+    private int high = 75;
+
+    public int getHigh() {
+        return high;
+    }
+
+    public void setHigh(int high) {
+        this.high = high;
+    }
+}
